dbcc3c301b3b871ca4324dbc7b8a9eee1f70d995
Rafael Barrios <rdbarrios20@hotmail.com>
2021-10-11 01:49:56 -0500

    Makiing the form login

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/index")
 @app.route("/")
-def main():
+def index():
     return render_template("index.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
