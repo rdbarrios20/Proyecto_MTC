0cdba4b3567a02edb35327925b37559edcd313cb
Rafael Barrios <rdbarrios20@hotmail.com>
2021-10-11 18:04:43 -0500

    Create the form to register the users

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/index")
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
-
 
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
